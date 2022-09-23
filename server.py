34c4417f0c783d81f3261fe6a97999b432ce6527
Seligenenko Andrey <andriik00@gmail.com>
2022-09-23 16:23:08 +0300

    test_1

---
+++ server.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+from flask import Flask, request
+app = Flask(__name__)
+@app.route('/post_json', methods=['POST'])
+def process_json():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        return json
+    else:
+        return 'Content-Type not supported!'
